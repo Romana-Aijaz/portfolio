fd6c5dccc6416f16d094b11b7701494664f191d4
Romana-Aijaz <romanaaijaz99@gmail.com>
2024-02-22 15:24:48 +0500

    home page in process

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './home.css'
+const Home = () => {
+  return (
+    <div className='main'>
+      <div className='title'>
+        <div className='heart-div'>
+        <img src='/assets/heart.svg' alt=''/>
+        </div>
+        <div className='letter-div'>M</div>
+      </div>
+      <div className='title'>I</div>
+      <div className='title'>S</div>
+      <div className='title'>H</div>
+      <div className='title'>E</div>
+    </div>
+  )
+}
+
+export default Home
